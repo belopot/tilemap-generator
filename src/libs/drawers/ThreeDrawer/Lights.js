13bbde1412ba0bff385383ff7dac44a16bb3029f
belopot <belopot@protonmail.com>
2022-11-10 22:08:45 +0800

    Update Lights.js

--- src/libs/drawers/ThreeDrawer/Lights.js
+++ src/libs/drawers/ThreeDrawer/Lights.js
@@ -11,26 +11,26 @@ class Lights extends THREE.Group {
     light1.position.set(SPACE_SIZE, SPACE_SIZE, SPACE_SIZE);
     light1.castShadow = false;
     light1.target.position.set(0, 0, 0);
-    this.add(light1);
+    // this.add(light1);
     this.ambientLight1 = light1;
 
     const light2 = new THREE.DirectionalLight(0xffe7df, 4);
     light2.position.set(-SPACE_SIZE, -SPACE_SIZE, -SPACE_SIZE);
     light2.castShadow = false;
     light2.target.position.set(0, 0, 0);
-    this.add(light2);
+    // this.add(light2);
     this.ambientLight2 = light2;
 
     //Sun
     const sunLight = new THREE.DirectionalLight(0x8e867c, 6);
     sunLight.position.set(0, SPACE_SIZE, 0);
-    sunLight.castShadow = true;
-    sunLight.shadow.mapSize.width = 512;
-    sunLight.shadow.mapSize.height = 512;
-    sunLight.shadow.bias = 0.001;
-    sunLight.shadow.blurSamples = 16;
-    sunLight.shadow.camera.near = 0.01;
-    sunLight.shadow.camera.far = SPACE_SIZE * 2;
+    sunLight.castShadow = false;
+    // sunLight.shadow.mapSize.width = 512;
+    // sunLight.shadow.mapSize.height = 512;
+    // sunLight.shadow.bias = 0.001;
+    // sunLight.shadow.blurSamples = 16;
+    // sunLight.shadow.camera.near = 0.01;
+    // sunLight.shadow.camera.far = SPACE_SIZE * 2;
     sunLight.target.position.set(0, 0, 0);
     this.add(sunLight);
     this.sunLight = sunLight;
