859ccd63ee570cc8ad35c3b6c9d208dedef82b20
belopot <belopot@protonmail.com>
2022-11-17 22:54:30 +0800

    Update ThreeDrawer.js

--- src/libs/drawers/ThreeDrawer/ThreeDrawer.js
+++ src/libs/drawers/ThreeDrawer/ThreeDrawer.js
@@ -492,10 +492,10 @@ export default class ThreeDrawer {
     // Draw
     this.drawTiles(dungeon.layers.tiles, Textures.tilesTextures(TEXTURE_ASSET));
     this.drawProps(dungeon.layers.props, Textures.propsTextures(TEXTURE_ASSET));
-    // this.drawMonsters(
-    //   dungeon.layers.monsters,
-    //   Textures.monstersTextures(TEXTURE_ASSET),
-    // );
+    this.drawMonsters(
+      dungeon.layers.monsters,
+      Textures.monstersTextures(TEXTURE_ASSET),
+    );
 
     // Fit camera
     FitCameraToSelection(
